f3411fc9be5323fa58303e35a2468edfcfcec058
Hüsamettin Demirtaş <husamdemirtas1@gmail.com>
2021-03-14 00:52:51 +0300

    features have been added
    so many features has been added

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Menu : MonoBehaviourPunCallbacks
+{
+    [Header("Screens")]
+    public GameObject mainScreen;
+    public GameObject lobbyScreen;
+
+    [Header("Main Screen")]
+    public Button createRoomButton;
+    public Button joinRoomButton;
+
+    [Header("Lobby Screen")]
+    public TextMeshProUGUI playerListText;
+    public Button startGameButton;
+
+    void Start()
+    {
+        // disable the buttons at the start we're not connected to the server yet
+        createRoomButton.interactable = false;
+        joinRoomButton.interactable = false;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        // called when we connect to the master server
+        // enables the "Create Room" and "Join Room" buttons
+        createRoomButton.interactable = true;
+        joinRoomButton.interactable = true;
+    }
+
+    void SetScreen (GameObject screen)
+    {
+        // deactive all screens
+        mainScreen.SetActive(false);
+        lobbyScreen.SetActive(false);
+
+        // enable the requested screen
+        screen.SetActive(true);
+    }
+
+    // called when the "Create Room" button is pressed
+    public void OnCreateRoomButton ( TMP_InputField roomNameInput)
+    {
+        NetworkManager.instance.CreateRoom(roomNameInput.text);
+    }
+
+    // called when the "Join Room" button is pressed
+    public void OnJoinRoomButton ( TMP_InputField roomNameInput)
+    {
+        NetworkManager.instance.JoinRoom(roomNameInput.text);
+    }
+
+    // called when we the player name input field has been updated
+    public void OnPlayerNameUpdate ( TMP_InputField playerNameInput)
+    {
+        PhotonNetwork.NickName = playerNameInput.text;
+    }
+
+    // called when we join a room
+    public override void OnJoinedRoom()
+    {
+        SetScreen(lobbyScreen);
+
+        // since there's a new player in the lobby, tell everyone to update the lobby
+        photonView.RPC("UpdateLobbyUI", RpcTarget.All);
+    }
+
+    public override void OnPlayerLeftRoom (Player otherPlayer)
+    {
+        // we dont RPC it like when we join the lobby
+        // that's because OnJoinedRoom is only called for the client who just joined to the lobby
+        // OnPlayerLeftRoom gets called for all clients in the room, so we don't need to call once more
+        UpdateLobbyUI();
+    }
+
+    // updates the lobby UI to show player list and host buttons
+    [PunRPC]
+    public void UpdateLobbyUI()
+    {
+        playerListText.text = "";
+        
+        // display all the players currently in the lobby
+        foreach(Player player in PhotonNetwork.PlayerList)
+        {
+            playerListText.text += player.NickName + "\n";
+        }
+
+        // only the host can start the game
+        if (PhotonNetwork.IsMasterClient)
+            startGameButton.interactable = true;
+        else
+            startGameButton.interactable = false;
+    }
+
+    public void OnLeaveLobbyButton ()
+    {
+        PhotonNetwork.LeaveRoom ();
+        SetScreen(mainScreen);
+    }
+
+    public void OnStartGameButton ()
+    {
+        NetworkManager.instance.photonView.RPC("ChangeScene", RpcTarget.All, "Game");
+    }
+}
---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // instance
+    public static NetworkManager instance;
+
+    void Awake()
+    {
+        // if an instance already exists and if it's not this one, destroy it
+        if (instance != null && instance != this)
+            gameObject.SetActive(false);
+        else
+        {
+            // set the instnace
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        
+    }
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+
+        
+    }
+
+   
+
+    // attempts to CREATE a room
+    public void CreateRoom (string roomName)
+    {
+        PhotonNetwork.CreateRoom(roomName);
+    }
+
+    
+
+    // attempts to JOIN a room
+    public void JoinRoom ( string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    // changes the scene using Photon's system
+    [PunRPC]
+    public void ChangeScene ( string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+
+
+
+}
