6fb7883a356ca20c63f08b46fe7b011c07c51502
Hüsamettin Demirtaş <husamdemirtas1@gmail.com>
2021-03-14 13:46:53 +0300

    hat swap
    hat spaw and so many features have been added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    [Header("Stats")]
+    public bool gameEnded = false;          // has game ended?
+    public float timeToWin;                 // time a player needs to hold the hat to win
+    public float invincibleDuration;       // how long after a player gets the hat
+    private float hatPickupTime;            // the time the hat was picked up by the current player
+
+    [Header("Players")]
+    public string playerPrefabLocation;
+    public Transform[] spawnPoints;
+    public PlayerController[] players;
+    public int playerWithHat;
+    private int playersInGame;
+
+    // instance
+    public static GameManager instance;
+
+    void Awake ()
+    {
+        // instance
+        instance = this;
+    }
+
+    void Start ()
+    {
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
+        
+    }
+
+    [PunRPC]
+    void ImInGame ()
+    {
+        playersInGame++;
+
+        // when all players are in the scene - spawn the players
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+            SpawnPlayer();
+    }
+
+    // spawns a player initializes it
+    void SpawnPlayer()
+    {
+        // instantiate the player across the network
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabLocation, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+
+        // get the player script
+        PlayerController playerScript = playerObj.GetComponent<PlayerController>();
+
+        // initialize the player
+        playerScript.photonView.RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
+    }
+
+    // returns the player who has the requested id
+    public PlayerController GetPlayer ( int playerId)
+    {
+        return players.First(x => x.id == playerId);
+    }
+
+    // returns the player of the requested GameObject
+    public PlayerController GetPlayer ( GameObject playerObj)
+    {
+        return players.First(x => x.gameObject == playerObj);
+    }
+
+
+    //called when a player hits the hattet player - giving them the hat
+    [PunRPC]
+    public void GiveHat (int playerId, bool initialGive)
+    {
+        // remove the hat from the currently hattet player
+        if (!initialGive)
+            GetPlayer(playerWithHat).SetHat(false);
+
+        // give the hat to the new player
+        playerWithHat = playerId;
+        GetPlayer(playerId).SetHat(true);
+
+        hatPickupTime = Time.time;
+    }
+
+    public bool CanGetHat ()
+    {
+        if(Time.time > hatPickupTime + invincibleDuration)
+            return true;
+        else
+            return false;
+    }
+
+    [PunRPC]
+    void WinGame(int playerId)
+    {
+        gameEnded = true;
+        PlayerController player = GetPlayer(playerId);
+
+        // set the ui to show who's won
+
+
+        Invoke("GoBackToMenu", 3.0f);
+
+    }
+
+    void GoBackToMenu()
+    {
+        PhotonNetwork.LeaveRoom();
+        NetworkManager.instance.ChangeScene("Menu");
+    }
+}
